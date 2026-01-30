87d424ba01a91a88971fdde28477bb9f0d128c53
maldoq <dominiquemalik20@gmail.com>
2026-01-30 12:10:50 +0000

    initialisation du deploiement

--- idsi_vote/settings.py
+++ idsi_vote/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from datetime import datetime
 import dj_database_url
-import environ
 import os
 
 from django.utils.timezone import make_aware
@@ -23,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-w716edh**74&gyfi)u*_ypr8zgdql=4r5q6w5#(_c^4kuhklwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
